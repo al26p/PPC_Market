f9a326e99ea47f28f03e29787a7853bda34c664a
Alban P <alban.prats@bde-insa-lyon.fr>
2019-01-11 10:57:17 +0100

    home

--- homes.py
+++ homes.py
@@ -1,13 +1,27 @@
+from multiprocessing import Process, Lock
+
+
 def Homes(weather, chan):
+    N = 10 # NOMBRE DE MAISONS
     lock = Lock()
     consomation = 0.0
     production = 0.0
     p_over_c = production / consomation
+    global wind
+    global sun
+    global temp
     wind = weather[0]
     sun = weather[1]
     temp = weather[2]
 
-    runThread(Home(lock))
+    home = list()
+    for i in range(N):
+        home.append(Process(target=Home, args=(lock,)))
+    for p in home:
+        p.start()
+    for p in home:
+        p.join()
+
 
 def Home(lock, time, c_initial, p_initial):
     while True:
