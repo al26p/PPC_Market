8270f2efae1cfc23f04d6d9d92345e1ec8635f7d
Aeredren <romain.lebrun-thauront@insa-lyon.fr>
2019-01-11 11:58:51 +0100

    improve weather and data

--- weather.py
+++ weather.py
@@ -1,10 +1,16 @@
-import random
-import time
+from time import sleep()
 
 
-def weather(array, refresh_interval=60):
+def weather(mutex, array, refresh_interval=10):
+    f=open("meteoData.txt", 'r')
     while True:
-        array[0] = random.randrange(0, 100) #wind
-        array[1] = random.randrange(0, 100) #sun
-        array[2] = random.randrange(220, 300) #temp in K
-        time.sleep(refresh_interval)
+        str = f.readline()
+        str = str.strip('\n')
+        str = str.split(' ')
+        with mutex :
+            array = str
+        print(array)
+        sleep(refresh_interval)
+
+if __name__ == "__main__" :
+    weather(list())
