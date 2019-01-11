68312a61e0b9e076961dfacb9c296b2358a0d66f
Alban P <alban.prats@bde-insa-lyon.fr>
2019-01-11 09:33:26 +0100

    refactor

--- external.py
+++ external.py
@@ -1,40 +1,39 @@
 # randomly send a signal 1 or 2 to the market, telling it an event occur.
 # sending a second time the same signal mean that the event end.
-
 import random
+from time import sleep
+from multiprocessing import Process
+import signal
+
 
 class External(Process):
-
     proba1 = 0.1
     proba2 = 0.05
-    crProba1 = 0.5
-    crProba1 = 0.3
-    p1
-    p2
+    cr_proba1 = 0.5
+    cr_proba1 = 0.3
 
-
-    def __init__ (time, marketPid)
+    def __init__(self, time, marketPid):
         super().__init__()
         self.time = time
         self.marketPid = marketPid
-        p1 = proba1
-        p2 = proba2
+        self.p1 = self.proba1
+        self.p2 = self.proba2
         random.seed()
 
-    def run (self) :
+    def run(self):
         while True :
-            wait (time)
-            i = random.random ()
-            j = random.random ()
-            if i < p1 :
-                if p1 == proba1 :
-                    p1 = crProba1
-                else :
-                    p1 = proba1
-                signal(SIGUSR1, marketPid)
-            if j < p2 :
-                if p2 == proba2 :
-                    p2 = crProba2
-                else :
-                    p2 = proba2
-                signal(SIGUSR2, marketPid)
+            sleep(self.time)
+            i = random.random()
+            j = random.random()
+            if i < self.p1:
+                if self.p1 == self.proba1:
+                    self.p1 = self.cr_proba1
+                else:
+                    self.p1 = self.proba1
+                signal.signal(signal.SIGUSR1, self.marketPid)
+            if j < self.p2:
+                if self.p2 == self.proba2:
+                    self.p2 = self.cr_proba2
+                else:
+                    self.p2 = self.proba2
+                signal.signal(signal.SIGUSR2, self.marketPid)
