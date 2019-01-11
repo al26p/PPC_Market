7805d0a06a125c561229f64604dd2978068c6ef8
Alban P <alban.prats@bde-insa-lyon.fr>
2019-01-11 17:26:28 +0100

    homes in progress

--- homes.py
+++ homes.py
@@ -1,40 +1,55 @@
-from multiprocessing import Process, Lock
+from multiprocessing import Process, Lock, Queue
+import queue
+import time
+from os import getpid
 
 
 COEF_TEMP = 0.2
 COEF_SUN = 0.1
 COEF_WIND = 0.08
+SELL = 0
+GIVE = 1
+TRYGIVE = 2
+TIMEOUT = 100
+
+
+class DispoEnergy:
+    def __init__(self, source, amount):
+        self.source = source
+        self.amount = amount
 
 
 def Homes(weather, chan):
     N = 10 # NOMBRE DE MAISONS
     lock = Lock()
-    consomation = 0.0
-    production = 0.0
-    p_over_c = production / consomation
     global wind
     global sun
     global temp
+    global energy
+    energy = 0.0
     wind = weather[0]
     sun = weather[1]
     temp = weather[2]
+    q = Queue()
 
+    q = queue.Queue()
     home = list()
     for i in range(N):
-        home.append(Process(target=Home, args=(lock,)))
+        home.append(Process(target=Home, args=(lock, q)))
     for p in home:
         p.start()
     for p in home:
         p.join()
 
 
-def Home(lock, time=60, c_initial=200, p_initial=100):
+def Home(lock, q, time=60, politic=SELL,  c_initial=200, p_initial=100):
     while True:
-        global consomation
-        global production
+        global energy
         energy_politic = 1  # ou 2 ou 3, dépend de s'il veut donner
-        consomation = time*(c_initial + temp*COEF_TEMP)
-        production = time*(p_initial + wind*COEF_WIND + sun*COEF_SUN)
+        energy_propre = - time*(c_initial + temp*COEF_TEMP) #conso
+        energy_propre += time*(p_initial + wind*COEF_WIND + sun*COEF_SUN) #prod
+        if politic != SELL and energy_propre > 0:
+            g = DispoEnergy(getpid(), energy_propre)
+            q.put(g, timeout=TIMEOUT)
         with lock:
-            consomation += consomation_propre
-            production += production_propre
+            energy += energy_propre
