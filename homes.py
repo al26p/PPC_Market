58310edafa70957a2b7a8c80dd374e098ec72bc5
Alban P <alban.prats@bde-insa-lyon.fr>
2019-01-18 11:45:43 +0100

    c bo

--- homes.py
+++ homes.py
@@ -20,35 +20,38 @@ class DispoEnergy:
         self.amount = amount
 
 
-def Homes(weather):
+def homes(weather):
     N = 10 # NOMBRE DE MAISONS
     lock = Lock()
     energy = Value('f')
     energy.value = 0
-    home = list()
+    hom = list()
     for i in range(N):
         c = random.randrange(0, 2000)
         p = random.randrange(0, 2000)
-        home.append(Process(target=Home, args=(lock, energy, weather, c, p)))
-    home.append(Process(target=show, args=(5, energy)))
-    for p in home:
+        hom.append(Process(target=home, args=(lock, energy, weather, c, p)))
+    hom.append(Process(target=show, args=(5, energy)))
+    for p in hom:
         p.start()
     print('Homes started')
-    for p in home:
+    for p in hom:
         p.join()
+    ptime.sleep(1)
     print('See ya !')
+
 
 def show(ttl, nrj):
     while True:
-        print(nrj.value)
+        print('Global energy', nrj.value)
         ptime.sleep(ttl)
 
+
 # begin with capitalism
-def Home(lock, energy, weather, c_initial=200, p_initial=100, time=60, politic=SELL):
+def home(lock, energy, weather, c_initial=200, p_initial=100, time=60, politic=SELL):
     while True:
         try:
             ptime.sleep(2)
-            energy_propre = - time*(c_initial + weather[0]*COEF_TEMP) #conso
+            energy_propre = - time*(c_initial + 1/weather[0]*COEF_TEMP) #conso
             energy_propre += time*(p_initial + weather[2]*COEF_WIND + weather[1]*COEF_SUN) #prod
             print('energy home', getpid(), energy_propre, 'meteo', weather[0])
             with lock:
@@ -60,7 +63,7 @@ def Home(lock, energy, weather, c_initial=200, p_initial=100, time=60, politic=S
 
 if __name__=='__main__':
     try:
-        Homes([1, 2, 10])
+        homes([1, 2, 10])
     except KeyboardInterrupt:
         print('exitting')
         ptime.sleep(2)
--- main.py
+++ main.py
@@ -2,20 +2,23 @@ from multiprocessing import Process, Array
 # import market
 import weather
 import homes
-
+import time
 
 if __name__ == '__main__':
-    print("Hello")
+    try:
+        print("Hello")
 
 
-    print('Initialization of the weather')
-    a = Array('f', range(3))
-    w = weather.Weather(a, 1)
-    h = Process(target=homes.Homes, args=(a,))
+        print('Initialization of the weather')
+        a = Array('f', range(3))
+        w = weather.Weather(a, 1)
+        h = Process(target=homes.homes, args=(a,))
 
-    w.start()
-    h.start()
-    print("gogogo")
-    w.join()
-    h.join()
+        w.start()
+        h.start()
+        print("gogogo")
+        w.join()
+        h.join()
+    except KeyboardInterrupt:
+        time.sleep(3)
     print("end")
