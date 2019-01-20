c6622e317d4d51c5f4d2ee32737a8fd278e59759
Alban P <alban.prats@gmail.com>
2019-01-20 21:25:30 +0100

    Lol àa marche pas. je comprends pas ce que ça fait mé c bo

--- market.py
+++ market.py
@@ -4,59 +4,61 @@ import threading
 import signal
 import sysv_ipc
 
-Y = 0.99 #long term attenuation coef
-S = -1000 #1 over the coef of the impact of the homes selling energy to the market
-B = 1000 #1 over the coef of the impact of the market selling energy to the homes
+Y = 0.99  # long term attenuation coef
+S = -1000  # 1 over the coef of the impact of the homes selling energy to the market
+B = 1000  # 1 over the coef of the impact of the market selling energy to the homes
 EXT_CTE1 = 0.5
 EXT_CTE2 = 1
 TIME = 60
 
-#average price of kW/h in France in centimes of € is 14.69c
+# average price of kW/h in France in centimes of € is 14.69c
 prix_prec = 14.69
 prix_actuel = 14.69
 
-
 # average energy consumption should be around 300 kW/h per house per month (per house per message)
-energy_sell = 0 #energy : home -> market
-energy_bought = 0 #energy : market -> home
+energy_sell = 0  # energy : home -> market
+energy_bought = 0  # energy : market -> home
 energy_mutex = threading.Lock()
 
-external_value1 =0
-external_value2 =0
+external_value1 = 0
+external_value2 = 0
 external1 = False
 external2 = False
-external_mutex = threading.Lock() #to protect the variable upside this line
+external_mutex = threading.Lock()  # to protect the variable upside this line
 
-class Market (multiprocessing.Process):
+
+class Market(multiprocessing.Process):
     def __init__(self, key, semaphore, time=60):
         super().__init__()
         global TIME
         TIME = time
         queue_semaphore = semaphore
         energy_queue = sysv_ipc.MessageQueue(key)
+
     def run(self):
         signal.signal(signal.SIGUSR1, handler)
         signal.signal(signal.SIGUSR2, handler)
         energy_thread = threading.Thread(target=gettingEnergy, args=(energy_queue, queue_semaphore,));
         price_thread = threading.Thread(target=CalculatingPrice)
 
-def gettingEnergy(energy_queue, queue_semaphore) :
+
+def gettingEnergy(energy_queue, queue_semaphore):
     global energy_mutex
     global energy_bought
     global energy_sell
 
-    while True :
+    while True:
         value = energy_queue.receive(type=1)
         queue_semaphore.release()
         value = float(value.decode())
-        with energy_mutex :
-            if 0 > value :
+        with energy_mutex:
+            if 0 > value:
                 energy_bought += value
-            else :
+            else:
                 energy_sell += value
 
 
-def CalculatingPrice () :
+def CalculatingPrice():
     global EXT_CTE1
     global EXT_CTE2
     global TIME
@@ -74,30 +76,29 @@ def CalculatingPrice () :
     global energy_sell
     global energy_mutex
 
-    while True :
-        sleep(TIME) # we re-calculate the price each "x" milisecond where x is TIME
+    while True:
+        sleep(TIME)  # we re-calculate the price each "x" milisecond where x is TIME
         # check if exceptionnal event
         with  external_mutex:
-            if external1 :
+            if external1:
                 external_value1 += EXT_CTE1
-            else :
+            else:
                 external_value1 = 0
 
-            if external2 :
+            if external2:
                 external_value2 += EXT_CTE2
-            else :
+            else:
                 external_value2 = 0
 
         with energy_mutex:
             prix_prec = prix_actuel
-            prix_actuel = Y*prix_prec + energy_sell/S + energy_bought/B + external_value1 + external_value2
-            energy_sell=0
-            energy_bought=0
+            prix_actuel = Y * prix_prec + energy_sell / S + energy_bought / B + external_value1 + external_value2
+            energy_sell = 0
+            energy_bought = 0
         print('Market Price :', prix_actuel)
 
 
-
-def handler(sig, frame) :
+def handler(sig, frame):
     global external1
     global external2
 
@@ -105,16 +106,16 @@ def handler(sig, frame) :
         if (not external1):
             print('wow ! Exceptional crisis 1')
             external1 = True
-        else :
+        else:
             print('End of the Exceptional crisis 1')
             external1 = False
 
     if sig == signal.SIGUSR2:
         if (not external2):
             print('wow ! Exceptional crisis 2')
-            with external_mutex :
+            with external_mutex:
                 external2 = True
-        else :
+        else:
             print('End of the Exceptional crisis 2')
-            with external_mutex :
+            with external_mutex:
                 external2 = False
