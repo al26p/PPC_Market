17f49285cc6eed7380dcedb66127f8d1e696dee3
romain <alban.prats@insa-lyon.fr>
2019-01-21 12:40:49 +0100

    main is ok

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from multiprocessing import Process, Array
+from multiprocessing import Process, Array, Semaphore
 # import market
 import weather
 import homes
+import market
 import time
 
 if __name__ == '__main__':
@@ -10,15 +11,19 @@ if __name__ == '__main__':
 
 
         print('Initialization of the weather')
+        queue_semaphore=multiprocessing.Semaphore(4)
         a = Array('f', range(3))
         w = weather.Weather(a, 1)
-        h = Process(target=homes.homes, args=(a,))
+        h = Process(target=homes.homes, args=(a,queue_semaphore,))
+        m = market.Market(queue_semaphore, 2)
 
+        m.start()
         w.start()
         h.start()
         print("gogogo")
         w.join()
         h.join()
+        m.join()
     except KeyboardInterrupt:
         time.sleep(3)
     print("end")
